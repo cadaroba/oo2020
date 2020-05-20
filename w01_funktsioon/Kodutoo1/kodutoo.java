135e0251ad2b4e54c8eb7c5dee942891c21aac94
Robin Kadakas <55129245+cadaroba@users.noreply.github.com>
2020-05-20 13:13:39 +0300

    Add files via upload

---
+++ w01_funktsioon/Kodutoo1/kodutoo.java
@@ -0,0 +1,41 @@
+public class kodutoo {
+
+    public static void ruutJoonis(){
+        for(int i=0;i<5;i++)	//või teha 2 for loopinga  
+        System.out.println(" * * * * * ");
+    }
+
+    public static void kolmnurkPool(){
+        for (int i = 0; i<5; i++){
+            for (int j=0; j<=i; j++){//kui j tuleb suurem kui i siis ei prindi
+                System.out.print(" *");
+            }
+            System.out.println();//sellega läheb uuele reale
+        }
+    }
+
+    public static void kolmnurkJoonis() {
+        for (int i = 0; i<5; i++) {//iga kord liidetakse +1 rida kuni 5
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print(" ");//nihutab poolikut kormnurka tühikutega paremale, kui see ära võtta tueb poolik kolmnurk
+            }
+            for (int k = 0; k<=i; k++) {
+                System.out.print("* ");// või System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.printf("%n");
+        System.out.println("Ruudu joonis tärnidega: ");
+        ruutJoonis();
+        System.out.printf("%n");
+        System.out.println("Pool kolmnurga joonist: ");
+        kolmnurkPool();
+        System.out.printf("%n");
+        System.out.println("Kolmnurga joonis tärnidega: ");
+        kolmnurkJoonis();
+        System.out.printf("%n");
+    }
+}
