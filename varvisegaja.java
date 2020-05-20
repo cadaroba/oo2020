f465623a5f656bfb5e42e8b8c3d4fcb2cc7eb552
Robin Kadakas <55129245+cadaroba@users.noreply.github.com>
2020-05-20 13:10:28 +0300

    Delete varvisegaja.java

--- varvisegaja.java
+++
@@ -1,8 +0,0 @@
-public class varvisegaja{
-    public static void main(String[] arg){
-        varvid varv1 = new varvid("kollane");
-        varvid varv2 = new varvid("punane");
-        varvid varv3 = varv1.segaVarvid(varv2);
-        //System.out.println(varv);
-    }
-}
